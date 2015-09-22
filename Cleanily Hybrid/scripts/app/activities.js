7b7f8ec9e1ad9ea1b1999a4567fb1288e365b65c
Quoc Nguyen <quocfreelancer@gmail.com>
2015-09-23 00:49:40 +0700

    - FIxed some layout. - Create mockup json data for customer. - Discuss with you as data structure - Research for drag n drop

--- Cleanily Hybrid/scripts/app/activities.js
+++ Cleanily Hybrid/scripts/app/activities.js
@@ -10,139 +10,133 @@ app.Activities = (function () {
     // Activities model
     var activitiesModel = (function () {
 
-        var activityModel = {
+        //var activityModel = {
 
-            id: 'Id',
-            fields: {
-                Text: {
-                    field: 'Text',
-                    defaultValue: ''
-                },
-                CreatedAt: {
-                    field: 'CreatedAt',
-                    defaultValue: new Date()
-                },
-                Picture: {
-                    fields: 'Picture',
-                    defaultValue: null
-                },
-                UserId: {
-                    field: 'UserId',
-                    defaultValue: null
-                },
-                Likes: {
-                    field: 'Likes',
-                    defaultValue: []
-                }
-            },
-            CreatedAtFormatted: function () {
+        //    id: 'Id',
+        //    fields: {
+        //        Text: {
+        //            field: 'Text',
+        //            defaultValue: ''
+        //        },
+        //        CreatedAt: {
+        //            field: 'CreatedAt',
+        //            defaultValue: new Date()
+        //        },
+        //        Picture: {
+        //            fields: 'Picture',
+        //            defaultValue: null
+        //        },
+        //        UserId: {
+        //            field: 'UserId',
+        //            defaultValue: null
+        //        },
+        //        Likes: {
+        //            field: 'Likes',
+        //            defaultValue: []
+        //        }
+        //    },
+        //    CreatedAtFormatted: function () {
 
-                return app.helper.formatDate(this.get('CreatedAt'));
-            },
-            PictureUrl: function () {
+        //        return app.helper.formatDate(this.get('CreatedAt'));
+        //    },
+        //    PictureUrl: function () {
 
-                return app.helper.resolvePictureUrl(this.get('Picture'));
-            },
-            User: function () {
+        //        return app.helper.resolvePictureUrl(this.get('Picture'));
+        //    },
+        //    User: function () {
 
-                //var userId = this.get('UserId');
+        //        //var userId = this.get('UserId');
 
-                //var user = $.grep(app.Users.users(), function (e) {
-                //    return e.Id === userId;
-                //})[0];
+        //        //var user = $.grep(app.Users.users(), function (e) {
+        //        //    return e.Id === userId;
+        //        //})[0];
 
-                //return user ? {
-                //    DisplayName: user.DisplayName,
-                //    PictureUrl: app.helper.resolveProfilePictureUrl(user.Picture)
-                //} : {
-                //    DisplayName: 'Anonymous',
-                //    PictureUrl: app.helper.resolveProfilePictureUrl()
-                //};
+        //        //return user ? {
+        //        //    DisplayName: user.DisplayName,
+        //        //    PictureUrl: app.helper.resolveProfilePictureUrl(user.Picture)
+        //        //} : {
+        //        //    DisplayName: 'Anonymous',
+        //        //    PictureUrl: app.helper.resolveProfilePictureUrl()
+        //        //};
 
-                return {
-                    DisplayName: 'Anonymous'
-                };
-            },
-            isVisible: function () {
-                //var currentUserId = app.Users.currentUser.data.Id;
-                //var userId = this.get('UserId');
+        //        return {
+        //            DisplayName: 'Anonymous'
+        //        };
+        //    },
+        //    isVisible: function () {
+        //        //var currentUserId = app.Users.currentUser.data.Id;
+        //        //var userId = this.get('UserId');
 
-                //return currentUserId === userId;
-                return true;
-            }
-        };
+        //        //return currentUserId === userId;
+        //        return true;
+        //    }
+        //};
 
         var show = function () {
             //// Activities data source. The Backend Services dialect of the Kendo UI DataSource component
             //// supports filtering, sorting, paging, and CRUD operations.
-            //var activitiesDataSource = new kendo.data.DataSource({
-            //    //type: 'everlive',
-            //    schema: {
-            //        model: activityModel
-            //    },
-            //    transport: {
-            //        // Required by Backend Services
-            //        typeName: 'Activities'
-            //    },
-            //    //change: function (e) {
-            //    //    app.mobileApp.hideLoading();
-            //    //    debugger;
-            //    //    if (e.items && e.items.length > 0) {
-            //    //        $('#no-activities-span').hide();
-            //    //    } else {
-            //    //        $('#no-activities-span').show();
-            //    //    }
-            //    //},
-            //    sort: { field: 'CreatedAt', dir: 'desc' }
-            //});
+            //    var data = app.everlive.data('Activities');
+            //    var query = new Everlive.Query();
+            //    app.mobileApp.showLoading();
+            //    data.get(query)
+            //        .then(function (data) {
+            //            if (data.result.length > 0) {
+            //                $('#no-activities-span').hide();
+            //                var dataSource = new kendo.data.DataSource({
+            //                    transport: {
+            //                        read: function (options) {
+            //                            try {
+            //                                app.mobileApp.hideLoading();
+            //                                options.success(data.result);
+            //                            } catch (err) {
+            //                                console.log(err);
+            //                            }
+            //                        }
+            //                    },
+            //                    error: function (e) {
+            //                        alert(JSON.stringify(error));
+            //                    },
+            //                    schema: { // describe the result format
+            //                        model: activityModel
+            //                    }
+            //                });
 
-
-            var data = app.everlive.data('Activities');
-            var query = new Everlive.Query();
-            data.get(query)
-                .then(function (data) {
-                    if (data.result.length > 0) {
-                        var dataSource = new kendo.data.DataSource({
-                            transport: {
-                                read: function (options) {
-                                    try {
-                                        options.success(data.result);
-                                    } catch (err) {
-                                        console.log(err);
-                                    }
-                                }
-                            },
-                            error: function (e) {
-                                alert(JSON.stringify(error));
-                            },
-                            schema: { // describe the result format
-                                model: activityModel
-                            }
-                        });
-
-                        $("#activities-listview").kendoMobileListView({
-                            dataSource: dataSource,
-                            template: $("#activityTemplate").html(),
-                            //appendOnRefresh: true
-                        });
-
-
-                    }
-                },
-                function (error) {
-                    alert(JSON.stringify(error));
+            //              
+            //            }
+            //            else {
+            //                $('#no-activities-span').show();
+            //            }
+            //        },
+            //        function (error) {
+            //            alert(JSON.stringify(error));
+            //        });
+            $.getJSON('/data/customers.json', function (data) {
+                //var dataSource = new kendo.data.DataSource({
+                //    transport: {
+                //        read: function (options) {
+                //            try {
+                //                app.mobileApp.hideLoading();
+                //                options.success(data);
+                //            } catch (err) {
+                //                console.log(err);
+                //            }
+                //        }
+                //    },
+                //    error: function (e) {
+                //        alert(JSON.stringify(error));
+                //    },
+                //    //schema: {
+                //    //    // describe the result format
+                //    //    model: activityModel
+                //    //}
+                //});
+                $("#activities-listview").kendoMobileListView({
+                    dataSource: data,
+                    template: $("#activityTemplate").html()
                 });
-
-            debugger;
-
-            //return {
-            //    activities: activitiesDataSource
-            //};
-
-
-
+            });
+           
         }
-
         return {
             show: show
         }
@@ -154,10 +148,9 @@ app.Activities = (function () {
 
         // Navigate to activityView When some activity is selected
         var activitySelected = function (e) {
-
-            app.mobileApp.navigate('views/activityView.html?uid=' + e.data.uid);
+            debugger;
+            app.mobileApp.navigate('views/activityView.html?uid=' + e);
         };
-
         // Navigate to app home
         var navigateHome = function () {
 
